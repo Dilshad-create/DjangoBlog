ab407255e5d3137e1c70ef7e86d7eb980cdc737c
Dilshad-create <mddilshadansari.mat18@itbhu.ac.in>
2021-09-29 18:01:05 +0530

    prohibited search of unpublihsed post_details by url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render,redirect
 from django.utils import timezone
 from .models import Post,Comment
@@ -13,7 +14,11 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if( post.published_date!=None ):
+        return render(request, 'blog/post_detail.html', {'post': post})
+    else:
+        return redirect('login')
+
 
 @login_required
 def post_new(request):
